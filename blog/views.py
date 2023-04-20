f728774317ee315c257441c691b37caa175b2771
evgenok12 <evgenok12@gmail.com>
2023-04-20 22:58:59 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -42,13 +42,13 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts__count,
     }
 
 
 def index(request):
     most_popular_posts = Post.objects.popular() \
-                                     .prefetch_related('author')[:5] \
+                                     .prefetch_related('author', 'tags')[:5] \
                                      .fetch_with_comments_count()
     
     fresh_posts = Post.objects.annotate(Count('comments')).order_by('-published_at')
